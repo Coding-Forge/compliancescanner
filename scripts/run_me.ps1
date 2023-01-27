3d586c3daa4f1c323eafc0612d7cb6c586ab3c2d
Brandon Campbell <brcampb@microsoft.com>
2023-01-27 13:26:53 -0500

    added changes

--- scripts/run_me.ps1
+++ scripts/run_me.ps1
@@ -2,7 +2,7 @@ $os = [System.Environment]::OSVersion.Platform
 
 if($os = "Unix"){
     Write-Host $os
-    ./export_logs -OutputPath "../logs"
+    ./export_logs.ps1 -OutputPath "../logs"
 
 } else {
     cd C:\Collector\Sentinel
