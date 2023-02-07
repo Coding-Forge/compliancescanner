e2ee252beb7eaa79701d2771b660e0721ece8b93
Brandon Campbell <brcampb@microsoft.com>
2023-02-07 14:44:08 -0500

    updated script with environment variables

--- scripts/certificate.ps1
+++ scripts/certificate.ps1
@@ -1,5 +1,5 @@
 # Create certificate
-$mycert = New-SelfSignedCertificate -DnsName "<dns name>" -CertStoreLocation "cert:\CurrentUser\My" -NotAfter (Get-Date).AddYears(1) -KeySpec KeyExchange
+$mycert = New-SelfSignedCertificate -DnsName $env:graph_tenant_domain -CertStoreLocation "cert:\CurrentUser\My" -NotAfter (Get-Date).AddYears(1) -KeySpec KeyExchange
 # Export certificate to .pfx file
 $mycert | Export-PfxCertificate -FilePath Sentinel.pfx -Password (Get-Credential).password
 # Export certificate to .cer file
